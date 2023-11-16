b55e46dff7b3fe0edf12db1eb3f24fb1015da861
fzucqq <147246130+fzucqq@users.noreply.github.com>
2023-11-16 20:27:24 +0800

    Add files via upload

---
+++ mine/mine.js
@@ -0,0 +1,90 @@
+// pages/indexx/indexx.js
+Page({
+   sell:function(){
+    wx.navigateTo({
+      url: '/pages/sell/sell'
+    })
+   },
+   donation:function(){
+    wx.navigateTo({
+      url: '/pages/donation/donation'
+    })
+   },
+   exchange:function(){
+    wx.navigateTo({
+      url: '/pages/exchange/exchange'
+    })
+   },
+   lease:function(){
+    wx.navigateTo({
+      url: '/pages/lease/lease'
+    })
+   },
+   Pop_ups:function(){
+    wx.navigateTo({
+      url: '/pages/Pop-ups/Pop-ups'
+    })
+   },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
